50ec83e03ac920bf59b99670520e886730684dc3
shawn peng <shawnpeng@yeah.net>
2021-06-03 20:19:00 +0800

    Add message recorder to save the message to help backtest

--- src/main/java/com/xronos/constants/MessageBoundPath.java
+++ src/main/java/com/xronos/constants/MessageBoundPath.java
@@ -2,5 +2,6 @@ package com.xronos.constants;
 
 public class MessageBoundPath {
   public static String OUTBOUND_MESSAGE = "XronosOutbound";
+  public static String OUTBOUND_HISTORICAL_MESSAGE = "XronosHistoricalOutbound";
   public static String INBOUND_MESSAGE = "XronosInbound";
 }
