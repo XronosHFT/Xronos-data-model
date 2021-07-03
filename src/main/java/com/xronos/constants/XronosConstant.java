906473934f025ef891b74132d4555b356ecbb17d
Shawn <shawnpeng@yeah.net>
2021-07-03 23:02:41 +0800

    add the lever rate values

--- src/main/java/com/xronos/constants/XronosConstant.java
+++ src/main/java/com/xronos/constants/XronosConstant.java
@@ -3,5 +3,13 @@ package com.xronos.constants;
 public class XronosConstant {
   public static final String UNDERLINE_SEPARATOR = "_";
   public static final String DOT_SEPARATOR = ".";
-
+  public static final int LEVER_RATE_1 = 5;
+  public static final int LEVER_RATE_5 = 5;
+  public static final int LEVER_RATE_10 = 5;
+  public static final int LEVER_RATE_20 = 5;
+  public static final int LEVER_RATE_30 = 5;
+  public static final int LEVER_RATE_50 = 5;
+  public static final int LEVER_RATE_70 = 5;
+  public static final int LEVER_RATE_100 = 5;
+  public static final int LEVER_RATE_125 = 5;
 }
