d855026b29e04e569e1215276b7501ad1340650f
Shawn <shawnpeng@yeah.net>
2022-10-29 12:00:36 +0800

    enhance

--- src/main/java/com/xronos/constants/XronosConstant.java
+++ src/main/java/com/xronos/constants/XronosConstant.java
@@ -21,38 +21,54 @@ public class XronosConstant {
 
   /***================核心持久化Map配置===============***/
   public static String CORE_MAP_PATH = "coreMap/";
+  public static String BACKTEST_MAP_TEMP_STRATEGY_PATH = "BackTestTrade/TempData/Strategy/";
+  public static String BACKTEST_MAP_TEMP_EXCHANGE_PATH = "BackTestTrade/TempData/Exchange/";
+  public static String BACKTEST_MAP_BASIC_STRATEGY_PATH = "BackTestTrade/BasicData/Strategy/";
 
   public static int TRADE_MAP_SIZE = 100_000;
   public static String TRADE_MAP_NAME = "Trade";
   public static String TRADE_MAP_PATH = CORE_MAP_PATH + "Trade.dat";
+  public static String BACKTEST_TRADE_MAP_STRATEGY_PATH = BACKTEST_MAP_TEMP_STRATEGY_PATH + "Trade.dat";
+  public static String BACKTEST_TRADE_MAP_EXCHANGE_PATH = BACKTEST_MAP_TEMP_EXCHANGE_PATH + "Trade.dat";
 
   public static int BALANCE_MAP_SIZE = 200;
   public static String BALANCE_MAP_NAME = "Balance";
   public static String BALANCE_MAP_PATH = CORE_MAP_PATH + "Balance.dat";
+  public static String BACKTEST_BALANCE_MAP_STRATEGY_PATH = BACKTEST_MAP_TEMP_STRATEGY_PATH + "Balance.dat";
+  public static String BACKTEST_BALANCE_MAP_EXCHANGE_PATH = BACKTEST_MAP_TEMP_EXCHANGE_PATH + "ExchangeBalance.dat";
 
   public static int POSITION_MAP_SIZE = 200;
   public static String POSITION_MAP_NAME = "Position";
   public static String POSITION_MAP_PATH = CORE_MAP_PATH + "Position.dat";
+  public static String BACKTEST_POSITION_MAP_STRATEGY_PATH = BACKTEST_MAP_TEMP_STRATEGY_PATH + "Position.dat";
+  public static String BACKTEST_POSITION_MAP_EXCHANGE_PATH = BACKTEST_MAP_TEMP_EXCHANGE_PATH + "ExchangePosition.dat";
 
   public static int ACCOUNT_MAP_SIZE = 100;
   public static String ACCOUNT_MAP_NAME = "Account";
   public static String ACCOUNT_MAP_PATH = CORE_MAP_PATH + "Account.dat";
+  public static String BACKTEST_ACCOUNT_MAP_STRATEGY_PATH = BACKTEST_MAP_TEMP_STRATEGY_PATH + "Account.dat";
+  public static String BACKTEST_ACCOUNT_MAP_EXCHANGE_PATH = BACKTEST_MAP_TEMP_EXCHANGE_PATH + "Account.dat";
 
   public static int ACTIVEORDER_MAP_SIZE = 10_000;
   public static String ACTIVEORDER_MAP_NAME = "ActiveOrder";
   public static String ACTIVEORDER_MAP_PATH = CORE_MAP_PATH + "ActiveOrder.dat";
+  public static String BACKTEST_ACTIVEORDER_MAP_STRATEGY_PATH = BACKTEST_MAP_TEMP_STRATEGY_PATH + "ActiveOrder.dat";
+  public static String BACKTEST_ACTIVEORDER_MAP_EXCHANGE_PATH = BACKTEST_MAP_TEMP_EXCHANGE_PATH + "ActiveOrder.dat";
 
   public static int INACTIVEORDER_MAP_SIZE = 100_000;
   public static String INACTIVEORDER_MAP_NAME = "InactiveOrder";
   public static String INACTIVEORDER_MAP_PATH = CORE_MAP_PATH + "InactiveOrder.dat";
+  public static String BACKTEST_INACTIVEORDER_MAP_STRATEGY_PATH = BACKTEST_MAP_TEMP_STRATEGY_PATH + "InactiveOrder.dat";
 
   public static int CONTRACT_MAP_SIZE = 10_000;
   public static String CONTRACT_MAP_NAME = "Contract";
   public static String CONTRACT_MAP_PATH = CORE_MAP_PATH + "Contract.dat";
+  public static String BACKTEST_CONTRACT_MAP_STRATEGY_PATH = BACKTEST_MAP_BASIC_STRATEGY_PATH + "Contract.dat";
 
   public static int IDEMPOTENT_MAP_SIZE = 100_000;
   public static String IDEMPOTENT_MAP_NAME = "Idempotent";
   public static String IDEMPOTENT_MAP_PATH = CORE_MAP_PATH + "Idempotent.dat";
+  public static String BACKTEST_IDEMPOTENT_MAP_STRATEGY_PATH = BACKTEST_MAP_TEMP_STRATEGY_PATH + "Idempotent.dat";
 
 
 }
