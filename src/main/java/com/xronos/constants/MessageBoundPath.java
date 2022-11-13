144e786e45f789f33ff525cabcc848aa78bfccc5
Shawn <shawnpeng@yeah.net>
2022-11-13 09:21:37 +0800

    enhance

--- src/main/java/com/xronos/constants/MessageBoundPath.java
+++ src/main/java/com/xronos/constants/MessageBoundPath.java
@@ -1,7 +1,15 @@
 package com.xronos.constants;
 
 public class MessageBoundPath {
-  public static String OUTBOUND_MESSAGE = "XronosOutbound";
-  public static String OUTBOUND_HISTORICAL_MESSAGE = "XronosHistoricalOutbound";
-  public static String INBOUND_MESSAGE = "XronosInbound";
+  private static String OUTBOUND_MESSAGE = "XronosOutbound";
+  private static String OUTBOUND_HISTORICAL_MESSAGE = "XronosHistoricalOutbound";
+  private static String INBOUND_MESSAGE = "XronosInbound";
+
+  public static String getInBoundMessage(String prefix) {
+    return prefix + INBOUND_MESSAGE;
+  }
+
+  public static String getOutBoundMessage(String prefix) {
+    return prefix + OUTBOUND_MESSAGE;
+  }
 }
