6ff6f70accdf19d1939f674d7ef663a3a1861e0d
shawn <shawnpeng@yeah.net>
2023-05-09 22:04:45 +0800

    enhance strategy

--- src/main/java/io/github/xronoshft/util/OrderIdUtils.java
+++ src/main/java/io/github/xronoshft/util/OrderIdUtils.java
@@ -2,10 +2,14 @@ package io.github.xronoshft.util;
 
 public class OrderIdUtils {
 
-    public static final String XS_PREFIX = "XS";
-    private static final SnowflakeIdWorker worker = new SnowflakeIdWorker(0,0);
+  public static final String XS_PREFIX = "XS";
+  private static final SnowflakeIdWorker worker = new SnowflakeIdWorker(0, 0);
 
-    public static Long generator() {
-        return worker.nextId();
-    }
+  public static Long generator() {
+    return worker.nextId();
+  }
+
+  public static Long getInitOrderId() {
+    return -1L;
+  }
 }
